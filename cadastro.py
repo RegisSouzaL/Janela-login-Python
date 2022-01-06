bfeda629071d08ab5b401d0d78aaff8b848e193a
RegisSouzaL <97205539+RegisSouzaL@users.noreply.github.com>
2022-01-06 00:58:58 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,19 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'), sg.Input(key='usuario')],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')],
+]
+
+janela = sg.Window('Tela de login', layout)
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'Regis' and valores['senha'] == '551912':
+            print('Meu primeiro projeto!')
